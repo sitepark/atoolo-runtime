64b239f8ac8d10d718ee9d5b19f9ff4d5edc148b
Holger Veltrup <veltrup@sitepark.com>
2024-06-17 11:56:03 +0200

    refactor: UmaskSetter validate before set

--- src/Executor/UmaskSetter.php
+++ src/Executor/UmaskSetter.php
@@ -8,6 +8,14 @@ use RuntimeException;
 
 class UmaskSetter implements RuntimeExecutor
 {
+    /**
+     * @var ?array{
+     *   value: int,
+     *   package: string
+     * }
+     */
+    private ?array $alreadySet = null;
+
     public function __construct(
         private readonly Platform $platform = new Platform()
     ) {
@@ -15,38 +23,51 @@ class UmaskSetter implements RuntimeExecutor
 
     public function execute(string $projectDir, array $options): void
     {
-        $alreadySet = null;
+        $umask = false;
         foreach ($options as $package => $packageOptions) {
             if (!isset($packageOptions['umask'])) {
                 continue;
             }
-            $value = $packageOptions['umask'];
-            if ($alreadySet !== null) {
-                $existsValue = $alreadySet['value'];
-                if ($existsValue === $value) {
-                    continue;
-                }
-                $package  = $alreadySet['package'];
-                throw new RuntimeException(
-                    "[atoolo.runtime.umask]: '
+            $value = $this->validate($package, $packageOptions['umask']);
+            if ($value !== false) {
+                $umask = $value;
+            }
+        }
+        if ($umask !== false) {
+            $this->platform->umask($umask);
+        }
+    }
+
+    private function validate(string $package, mixed $value): false|int
+    {
+        if (!is_numeric($value)) {
+            throw new RuntimeException(
+                "[atoolo.runtime.umask]: '
+                    . 'umask must be an integer: "
+                . $value
+            );
+        }
+
+        $umask = (int)$value;
+
+        if ($this->alreadySet !== null) {
+            $existsValue = $this->alreadySet['value'];
+            if ($existsValue === $umask) {
+                return false;
+            }
+            $package  = $this->alreadySet['package'];
+            throw new RuntimeException(
+                "[atoolo.runtime.umask]: '
                     . 'umask is already set to $existsValue '
                     . ' for package $package"
-                );
-            }
-
-            if (!is_numeric($value)) {
-                throw new RuntimeException(
-                    "[atoolo.runtime.umask]: '
-                    . 'umask must be an integer: "
-                    . $value
-                );
-            }
-
-            $this->platform->umask((int)$value);
-            $alreadySet = [
-                'value' => $value,
-                'package' => $package
-            ];
+            );
         }
+
+        $this->alreadySet = [
+            'value' => $umask,
+            'package' => $package
+        ];
+
+        return $umask;
     }
 }
--- test/Executor/UmaskSetterTest.php
+++ test/Executor/UmaskSetterTest.php
@@ -4,54 +4,65 @@ declare(strict_types=1);
 
 namespace Atoolo\Runtime\Test\Executor;
 
+use Atoolo\Runtime\Executor\Platform;
 use Atoolo\Runtime\Executor\UmaskSetter;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
 use RuntimeException;
 
 #[CoversClass(UmaskSetter::class)]
 class UmaskSetterTest extends TestCase
 {
-    private int $originUmask;
-
-    public function setUp(): void
-    {
-        $this->originUmask = umask();
-    }
-
-    public function tearDown(): void
-    {
-        umask($this->originUmask);
-    }
-
+    /**
+     * @throws Exception
+     */
     public function testSetUmask(): void
     {
-        $umaskSetter = new UmaskSetter();
+        $platform = $this->createMock(Platform::class);
+        $umaskSetter = new UmaskSetter($platform);
+
+        $platform->expects($this->once())
+            ->method('umask')
+            ->with(123)
+            ->willReturn(123);
         $umaskSetter->execute('', [
             'package1' => [
                 'umask' => '0123'
             ]
         ]);
-        $this->assertEquals(
-            123,
-            umask(),
-            'The umask should have been set'
-        );
     }
 
+    /**
+     * @throws Exception
+     */
     public function testSetUmaskWithoutUmask(): void
     {
-        $umaskSetter = new UmaskSetter();
-        $this->expectNotToPerformAssertions();
+        $platform = $this->createMock(Platform::class);
+        $umaskSetter = new UmaskSetter($platform);
+
+        $platform->expects($this->never())
+            ->method('umask');
+
         $umaskSetter->execute('', [
             'package1' => [
             ]
         ]);
     }
 
+    /**
+     * @throws Exception
+     */
     public function testSetUmaskTwiceWithSameValue(): void
     {
-        $umaskSetter = new UmaskSetter();
+        $platform = $this->createMock(Platform::class);
+        $umaskSetter = new UmaskSetter($platform);
+
+        $platform->expects($this->once())
+            ->method('umask')
+            ->with(123)
+            ->willReturn(123);
+
         $umaskSetter->execute('', [
             'package1' => [
                 'umask' => '0123'
@@ -60,16 +71,13 @@ class UmaskSetterTest extends TestCase
                 'umask' => '0123'
             ]
         ]);
-        $this->assertEquals(
-            123,
-            umask(),
-            'The umask should have been set'
-        );
     }
 
     public function testSetUmaskTwiceWithDifferentValues(): void
     {
+
         $umaskSetter = new UmaskSetter();
+
         $this->expectException(RuntimeException::class);
         $umaskSetter->execute('', [
             'package1' => [
