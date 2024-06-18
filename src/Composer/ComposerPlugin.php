43351e12f164d56399dc83b70d65fc9d01df5525
Holger Veltrup <92872893+sitepark-veltrup@users.noreply.github.com>
2024-06-18 13:31:43 +0200

    Feature/initial implementation (#2)
    feat: initial implementation
    
    ---------
    
    Co-authored-by: Mario Schäper <95750382+sitepark-schaeper@users.noreply.github.com>

--- src/Composer/ComposerPlugin.php
+++ src/Composer/ComposerPlugin.php
@@ -6,21 +6,78 @@ namespace Atoolo\Runtime\Composer;
 
 use Composer\Composer;
 use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Factory;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\ScriptEvents;
+use InvalidArgumentException;
+use JsonException;
 
 class ComposerPlugin implements PluginInterface, EventSubscriberInterface
 {
-    private Composer $composer;
     private IOInterface $io;
+
+    private RuntimeFile $runtimeFile;
+
+    private ComposerJson $composerJson;
+
+    private ComposerJsonFactory $composerJsonFactory;
+
+    private RuntimeFileFactory $runtimeFileFactory;
+
     private static bool $activated = false;
 
+    public function __construct(
+        ?ComposerJsonFactory $composerJsonFactory = null,
+        ?RuntimeFileFactory $runtimeFileFactory = null
+    ) {
+        $this->composerJsonFactory = $composerJsonFactory
+            ?? new ComposerJsonFactory();
+        $this->runtimeFileFactory = $runtimeFileFactory
+            ?? new RuntimeFileFactory();
+    }
+
+    public static function getSubscribedEvents()
+    {
+        if (!self::$activated) {
+            return [];
+        }
+
+        $method = 'updateRuntime';
+        $priority = 1;
+        return [
+            ScriptEvents::PRE_AUTOLOAD_DUMP => [$method, $priority]
+        ];
+    }
+
+    /**
+     * @throws JsonException
+     */
     public function activate(Composer $composer, IOInterface $io): void
     {
-        self::$activated = true;
-        $this->composer = $composer;
         $this->io = $io;
+        $this->composerJson = $this->composerJsonFactory->create(
+            $composer,
+            Factory::getComposerFile()
+        );
+        $this->runtimeFile = $this->runtimeFileFactory->create(
+            $composer,
+            dirname($this->composerJson->getPath())
+        );
+        self::$activated = true;
+    }
+
+    /**
+     * @throws JsonException
+     * @throws InvalidArgumentException
+     *  if the configured template file does not exist
+     */
+    public function updateRuntime(): void
+    {
+        $this->runtimeFile->updateRuntimeFile($this->io);
+        $this->composerJson->addAutoloadFile(
+            $this->runtimeFile->getRuntimeFilePath()
+        );
     }
 
     public function deactivate(Composer $composer, IOInterface $io)
@@ -30,21 +87,9 @@ class ComposerPlugin implements PluginInterface, EventSubscriberInterface
 
     public function uninstall(Composer $composer, IOInterface $io)
     {
-        //@unlink($composer->getConfig()->get('vendor-dir').'/autoload_runtime.php');
-    }
-
-    public function updateAutoloadFile(): void
-    {
-    }
-
-    public static function getSubscribedEvents()
-    {
-        if (!self::$activated) {
-            return [];
-        }
-
-        return [
-            ScriptEvents::POST_AUTOLOAD_DUMP => 'updateAutoloadFile',
-        ];
+        $this->runtimeFile->removeRuntimeFile($this->io);
+        $this->composerJson->removeAutoloadFile(
+            $this->runtimeFile->getRuntimeFilePath()
+        );
     }
 }
